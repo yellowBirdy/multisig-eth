cfb01f44a823848b11e0a017912b18e3fb43a8db
Mieszko Manijak <mieszko@grit42.com>
2021-02-18 15:24:50 +0100

    metamask in

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,24 @@ import Web3 from "web3";
 import Wallet from "./contracts/Wallet.json";
 
 const getWeb3 = () => {
-    return new Web3("http://127.0.0.1:9545");
+//    return new Web3("http://127.0.0.1:9545");
+    return new Promise( (resolve, reject) => {
+        window.addEventListener("load", async () => {
+            if (window.ethereum) {
+                try {
+                    const web3 = new Web3(window.ethereum)
+                    await window.ethereum.enable()
+                    resolve(web3)
+                } catch (err) {
+                    reject(err)
+                }
+            } else if (window.web3) {
+                resolve(window.web3)
+            } else {
+                reject("Multisig: no wallet available.")
+            }
+        })
+    })
 }
 
 const _getContract = async (web3, artifact) => {
